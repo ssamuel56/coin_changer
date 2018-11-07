24a8fa66a937d8fac4c25941492d60aa4c88cf33
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-11-07 14:37:38 -0500

    Values are now being added to the hash

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,9 +1,12 @@
 def coinify(change)
   hash = Hash.new
+  quarter = 0; dime = 0; nickel = 0; penny = 0;
   while change > 0
-    if change % 25 >= 1
-      hash.merge(:Quarter => quarter += 1)
+    if change / 25 >= 1
+      hash[:Quarter] = quarter += 1
+      change -= 25
     end
   end
+  p hash
   return hash
 end
