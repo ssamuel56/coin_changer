a62a53484bf5b5239c4eeec64397fa701b5331f0
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-11-09 21:50:36 -0500

    Added plural to quarter if more than one with a one liner if statement

--- coin_changer.rb
+++ coin_changer.rb
@@ -3,8 +3,8 @@ def coinify(change)
   quarter = 0; dime = 0; nickel = 0; penny = 0;
   while change > 0
     if change / 25 >= 1
-      hash[:Quarter] = (change / 25).to_i
-      change -= (25 * hash[:Quarter]).to_i
+      hash[change / 25 > 1 ? :Quarters : :Quarter] = (change / 25).to_i
+      change -= (25 * hash[change / 25 > 1 ? :Quarters : :Quarter]).to_i
     elsif change / 10 >= 1
       hash[:Dime] = dime += 1
       change -= 10
--- coin_changer_tdd.rb
+++ coin_changer_tdd.rb
@@ -9,7 +9,7 @@ class Test_for_coin_changer < Minitest::Test
     assert_equal({:Quarter=>1}, coinify(25))
   end
   def test_for_two_quarters
-    assert_equal({:Quarter=>2}, coinify(50))
+    assert_equal({:Quarters=>2}, coinify(50))
   end
   def test_for_a_dime
     assert_equal({:Dime=>1}, coinify(10))
@@ -27,22 +27,22 @@ class Test_for_coin_changer < Minitest::Test
     assert_equal({:Penny=>1}, coinify(1))
   end
   def test_for_2_quarters_a_dime_a_nickel_and_4_pennies
-    assert_equal({:Quarter=>2, :Dime=>1, :Nickel=>1, :Penny=>4}, coinify(69))
+    assert_equal({:Quarters=>2, :Dime=>1, :Nickel=>1, :Penny=>4}, coinify(69))
   end
   def test_for_95
-    assert_equal({:Quarter=>3, :Dime=>2}, coinify(95))
+    assert_equal({:Quarters=>3, :Dime=>2}, coinify(95))
   end
   def test_for_632
-    assert_equal({:Quarter=>25, :Nickel=>1, :Penny=>2}, coinify(632))
+    assert_equal({:Quarters=>25, :Nickel=>1, :Penny=>2}, coinify(632))
   end
   def test_for_3
     assert_equal({:Penny=>3}, coinify(3))
   end
   def test_for_99
-    assert_equal({:Quarter=>3, :Dime=>2, :Penny=>4}, coinify(99))
+    assert_equal({:Quarters=>3, :Dime=>2, :Penny=>4}, coinify(99))
   end
   def test_for_4969879533
-    assert_equal({:Quarter=>198795181, :Nickel=>1, :Penny=>3}, coinify(4969879533))
+    assert_equal({:Quarters=>198795181, :Nickel=>1, :Penny=>3}, coinify(4969879533))
   end
   def test_for_loop_one_quarter
     assert_equal({:Quarter=>1}, coinify_loop(25))
